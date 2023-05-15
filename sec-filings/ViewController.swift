7e298bd6fc97d17ace148cf6ae5236a3c62b7d44
Aramis Tanelus <aramis.tanelus@gmail.com>
2023-05-15 13:37:44 -0400

    Initial Commit

---
+++ sec-filings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sec-filings
+//
+//  Created by Aramis Tanelus on 5/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
